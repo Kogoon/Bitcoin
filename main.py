7eda5bc64fdaead7a605cbf503df56f3806307b3
Kogoon <gojs712@gmail.com>
2021-06-01 08:24:26 +0900

    Apply render_template

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template, abort
+from flask import Flask, Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_page = Blueprint('simple_page', __name__,
                         template_folder='templates')
 
-@simple_page.route('/', defaults={'page': 'index'})
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
 @simple_page.route('/<page>')
 def show(page):
     try:
@@ -13,4 +18,4 @@ def show(page):
         abort(404)
 
 if __name__ == "__main__":
-    simple_page.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
